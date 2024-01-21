84c9a48a3672012095863be274349a39b85f888f
Devipriya07 <devipriya9512@gmail.com>
2024-01-22 03:21:04 +0530

    hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import React from 'react'
 import NavBar from './components/NavBar/NavBar.js'
+import Hero from './components/Hero/Hero.js'
+
 
 function App() {
   return (
     <>
       <NavBar />
+      <Hero />
     </>
   )
 }
